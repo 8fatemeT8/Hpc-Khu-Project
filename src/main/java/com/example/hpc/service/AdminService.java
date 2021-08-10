e8e404327a21b636f0bf4e1918205d55c16a7ea1
f.tork <f.tork@yaas.bm>
2021-08-10 11:07:45 +0430

    set CreateDate and UpdateDate

--- src/main/java/com/example/hpc/service/AdminService.java
+++ src/main/java/com/example/hpc/service/AdminService.java
@@ -5,13 +5,27 @@ import com.example.hpc.model.dto.AdminDto;
 import com.example.hpc.model.entity.Admin;
 import com.example.hpc.model.repository.AdminRepository;
 import com.example.hpc.service.bases.ServiceBase;
+import com.example.hpc.utils.hooks.BeforeAdd;
+import com.example.hpc.utils.hooks.BeforeUpdate;
 import com.example.hpc.utils.mapper.AdminMapper;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+
 @Service
-public class AdminService extends ServiceBase<Admin, AdminDto, AdminDomain, AdminRepository, AdminMapper> {
+public class AdminService extends ServiceBase<Admin, AdminDto, AdminDomain, AdminRepository, AdminMapper> implements BeforeAdd<Admin, AdminDto>, BeforeUpdate<Admin,AdminDto> {
 
     public AdminService(AdminRepository adminRepository, AdminMapper adminMapper) {
         super(adminRepository, adminMapper);
+    }
+
+    @Override
+    public void add(AdminDto adminDto, Admin admin) {
+        admin.setCreateDate(Instant.now());
+    }
+
+    @Override
+    public void update(AdminDto adminDto, Admin admin) {
+        admin.setUpdateDate(Instant.now());
     }
 }
--- src/main/java/com/example/hpc/service/WalletDetailService.java
+++ src/main/java/com/example/hpc/service/WalletDetailService.java
@@ -6,16 +6,30 @@ import com.example.hpc.model.entity.WalletDetail;
 import com.example.hpc.model.repository.WalletDetailRepository;
 import com.example.hpc.service.bases.ServiceWithSearchBase;
 import com.example.hpc.utils.criteria.WalletDetailCriteria;
+import com.example.hpc.utils.hooks.BeforeAdd;
+import com.example.hpc.utils.hooks.BeforeUpdate;
 import com.example.hpc.utils.mapper.WalletDetailMapper;
 import com.example.hpc.utils.predicates.WalletDetailPredicate;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+
 @Service
 public class WalletDetailService extends ServiceWithSearchBase<WalletDetail, WalletDetailDto,
-		WalletDetailDomain, WalletDetailRepository, WalletDetailMapper, WalletDetailCriteria, WalletDetailPredicate> {
+		WalletDetailDomain, WalletDetailRepository, WalletDetailMapper, WalletDetailCriteria, WalletDetailPredicate> implements BeforeAdd<WalletDetail,WalletDetailDto>, BeforeUpdate<WalletDetail,WalletDetailDto> {
 
 	public WalletDetailService(WalletDetailRepository walletDetailRepository,
 							   WalletDetailMapper walletDetailMapper, WalletDetailPredicate walletDetailPredicate) {
 		super(walletDetailRepository, walletDetailMapper, walletDetailPredicate);
+	}
+
+	@Override
+	public void add(WalletDetailDto walletDetailDto, WalletDetail walletDetail) {
+		walletDetail.setCreateDate(Instant.now());
+	}
+
+	@Override
+	public void update(WalletDetailDto walletDetailDto, WalletDetail walletDetail) {
+		walletDetail.setUpdateDate(Instant.now());
 	}
 }
